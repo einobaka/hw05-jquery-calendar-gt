fd2dcfd3e8c44bd68c9807bc5e5a27a8fa7e75f8
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-02 19:59:25 -0700

    succesfully made the lists with the proper iDs based on the timeSlot array

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// conditional for the hours // put in a function later
+if (timeNowin24 <= 12) {
+    var period = "AM";
+}
+else {
+    var period = "PM";
+}
 
 // console.log(timeStamp);
 
@@ -6,7 +13,8 @@ var time = moment().format('LLLL');
 // Time now in 24 hr format
 var timeNowin24 = moment().format('k');
 // Time now in 12 hr format
-var timeNowin12 = moment().format('h');
+// var timeNowin12 = moment().format('h');
+var timeNowin12 = 7;
 console.log(timeNowin24);
 console.log(timeNowin12);
 
@@ -29,34 +37,35 @@ timeEvents = {
 
 };
 
-// conditional for the hours // put in a function later
-if (timeNowin24 <= 12) {
-    var period = "AM";
-}
-else {
-    var period = "PM";
-}
+
 
 // Build list of schedule slots
-timeSlot = ["7AM ", "8AM ", "9AM ", "10AM ", "11AM ", "12PM ", "1PM ", "2PM ", "3PM ", "4PM ", "5PM "]
+timeSlot = [7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5]
 
 for (var i = 0; i < timeSlot.length; i++) {
 
-
-    var setID = "#id=" + timeSlot[i];
     var scheduleSlot = timeSlot[i];
     var li = $("<li>");
-    console.log(scheduleSlot);
+    $(li).attr("class", "list-group-item list-group-item-dark")
+    $(li).attr("id", scheduleSlot);
+    if (scheduleSlot === timeNowin12) {
+        $(li).attr("class", "list-group-item list-group-item-success")
+    }
+    else if (scheduleSlot > timeNowin12) {
+        $(li).attr("class", "list-group-item list-group-item-warning")
+    };
 
     $(li).text(scheduleSlot);
     $(".container").append(li);
 
 
-    if ($("id") === timeNowin12) {
-        // make the list green?
-        $(".time").attr("class", "alert alert-success")
-    }
+
+    // if ($("id") === timeNowin12) {
+    //     // make the list green?
+    //     $(".time").attr("class", "alert alert-success")
+    // }
 };
+console.log(scheduleSlot);
 // console.log($(".container"))
 // console.log(period);
 
