99660451af8726e06ccca203cac5c6a79d7415f5
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-05 12:40:41 -0700

    reworked major overhaul on for loop for the task inputs

--- script.js
+++ script.js
@@ -19,31 +19,39 @@ var timeNowin24 = moment().format('k');
 // Test variable for when real time is not in sync to demo the list color coordination
 // var timeNowin24 = 9;
 
-// List of schedule slots
-timeSlot = ["7AM", "8AM ", "9AM", "10AM", "11AM", "12PM", "1PM ", "2PM ", "3PM ", "4PM ", "5PM "]
+// // List of schedule slots
+timeSlot = ["07 AM", "08 AM ", "09 AM", "10 AM", "11 AM", "12 PM", "01 PM", "02 PM", "03 PM", "04 PM", "05 PM"]
 timeLock = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 for (var i = 0; i < timeSlot.length; i++) {
 
     var scheduleSlot = timeSlot[i];
     var scheduleLock = timeLock[i];
-    var li = $("<li>");
-    var divCol = $("<div>");
-    var divColNext = $("<span>");
+
+    var divPrime = $("<div>");
+    var divSub = $("<div>");
+    var divSub2 = $("<div>");
+    var newSpan = $("<span>");
+    var newInput = $("<input>");
+    var newButton = $("<button>");
+
+    // Main append block
+    $("#timeBlock").append(divPrime);
+    $(divPrime).attr("class", "input-group").append(divSub).append(newInput).append(divSub2);
+    // Attribute assignment block
+    $(divSub).attr("class", "input-group-prepend").append(newSpan);
+    $(newSpan).attr("class", "input-group-text").text(scheduleSlot);
+    $(newInput).attr("type", "text").attr("class", "form-control alert-dark").attr("placeholder", "").attr("id", scheduleLock);
+    $(divSub2).attr("class", "input-group-append").append(newButton);
+    $(newButton).attr("class", "btn btn-outline-secondary").attr("type", "button").text("SAVE");
 
     // List attributes
-    $(li).attr("class", "row text-left list-group-item list-group-item-action list-group-item-dark border border-dark").attr("id", scheduleLock)
-    $(li).append(divCol);
-    $(divCol).attr("class", "col-sm").text(scheduleSlot).attr("id", scheduleSlot);
-    // $(li).append(input).append(save);
-
     if (scheduleLock == timeNowin24) {
-        $(li).attr("class", "row text-left list-group-item list-group-item-action list-group-item-success border border-dark")
+        $(newInput).attr("type", "text").attr("class", "form-control alert-success").attr("placeholder", "")
     }
     else if (scheduleLock > timeNowin24) {
-        $(li).attr("class", "row text-left list-group-item list-group-item-action list-group-item-warning border border-dark")
+        $(newInput).attr("type", "text").attr("class", "form-control alert-warning").attr("placeholder", "")
     };
-    $("#schedule").append(li);
 
 };
 
