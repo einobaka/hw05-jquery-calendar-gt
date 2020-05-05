00ef6c239fe97976ab11b1f648fce0916367cbf6
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-04 17:24:15 -0700

    been making several commits to the incorrect repo, commit count is down at least 5 commits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// conditional for the hours // put in a function later
+// Conditional for the hours to determine AM or PM 
 if (timeNowin24 <= 12) {
     var period = "AM";
 }
@@ -6,22 +6,22 @@ else {
     var period = "PM";
 }
 
-// console.log(timeStamp);
-
+// The current day and time
 var time = moment().format('LLLL');
-
-// Time now in 24 hr format
-// var timeNowin24 = moment().format('k');
-// Time now in 12 hr format
-var timeNowin12 = moment().format('h');
-var timeNowin24 = 9;
-console.log(timeNowin24);
-console.log(timeNowin12);
-
 $("#currentDay").text(time);
 
+// Time now in 12 hr format
+var timeNowin12 = moment().format('h');
 
-// Build list of schedule slots
+// Time now in 24 hr format
+var timeNowin24 = moment().format('k');
+
+// Test variable
+// var timeNowin24 = 9;
+console.log(timeNowin24);
+// console.log(timeNowin12);
+
+// List of schedule slots
 timeSlot = ["7AM", "8AM ", "9AM", "10AM", "11AM", "12PM", "1PM ", "2PM ", "3PM ", "4PM ", "5PM "]
 timeLock = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
@@ -30,36 +30,57 @@ for (var i = 0; i < timeSlot.length; i++) {
     var scheduleSlot = timeSlot[i];
     var scheduleLock = timeLock[i];
     var li = $("<li>");
-    var save = $("<button>");
-
-    // When clicked then it becomes an input
-    // var enterTask = $("<input>");
-    // $(enterTask).attr("class", "form-control").attr("id", scheduleLock).attr("placeholder", "What is the task?");
-    // $(save).attr("class", "btn btn-info").text("SAVE");
+    var input = $("<input>").attr("id", "task");
+    var save = $("<button>").attr("id", "save");
+    $(save).text("SAVE");
 
     // List attributes
-    $(li).attr("class", "text-left list-group-item list-group-item-dark").attr("id", scheduleLock)
-    $(li).text(scheduleSlot);
+    $(li).attr("class", "text-left list-group-item list-group-item-action list-group-item-dark border border-dark").attr("id", scheduleLock)
+    $(li).text(scheduleSlot + " ");
+    // $(li).append(input).append(save);
 
-
-    if (scheduleLock === timeNowin24) {
-        $(li).attr("class", "text-left list-group-item list-group-item-success")
+    if (scheduleLock == timeNowin24) {
+        $(li).attr("class", "text-left list-group-item list-group-item-action list-group-item-success border border-dark")
     }
     else if (scheduleLock > timeNowin24) {
-        $(li).attr("class", "text-left list-group-item list-group-item-warning")
+        $(li).attr("class", "text-left list-group-item list-group-item-action list-group-item-warning border border-dark")
     };
-
-    // hide and appear only when task is clicked?
-
     $("#schedule").append(li);
-    // $(li).append(enterTask).append(save);
-
-    // if ($("id") === timeNowin12) {
-    //     // make the list green?
-    //     $(".time").attr("class", "alert alert-success")
-    // }
 
 };
+
+// var inPlace = $("<input>")
+// var inSave = $("<button>")
+// $("#input").append(inPlace);
+// $("#save").append(inSave);
+// $(inSave).text("SAVE");
+
+// $("#list").on("click", function () {
+//     function listIt() {
+//         $(this).append("<span>");
+//         $("span").attr("id", "task");
+//         $("#task").append(input).append(save);
+//     };
+//     listIt()
+// });
+
+$("li").on("click", function (e) {
+    e.preventDefault();
+    console.log(e)
+    if (e.target.matches(".list-group-item")) {
+        $(this).append(input).append(save);
+    };
+
+});
+
+
+// $("li").on("click", function (e) {
+//     e.preventDefault();
+//     console.log(e);
+//     input.show();
+//     save.show();
+// });
+
 console.log(scheduleSlot);
 // console.log($(".container"))
 // console.log(period);
